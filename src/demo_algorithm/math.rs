f3f6d30b7f52a5f506109f2847f8be6e98858f44
easepan <easepan@qq.com>
2024-07-30 16:44:24 +0800

    feat: resolve leetcode problem 2961

--- src/demo_algorithm/math.rs
+++ src/demo_algorithm/math.rs
@@ -11,4 +11,25 @@ impl Solution {
         let n = num as usize;
         format!("{}{}{}{}", THOUSANDS[n / 1000], HUNDREDS[n / 100 % 10], TENS[n / 10 % 10], ONES[n % 10])
     }
+
+    /// LeetCode(id=2961,title=双模幂运算,difficulty=medium)
+    pub fn get_good_indices(variables: Vec<Vec<i32>>, target: i32) -> Vec<i32> {
+        let pow = |mut x, mut n, m| {
+            // 本题 m 很小，即使平方也不会超过 i32 范围，所以不需要用 i64
+            let mut res = 1;
+            while n > 0 {
+                if n & 1 == 1 {
+                    res = res * x % m;
+                }
+                x = x * x % m;
+                n >>= 1;
+            }
+            res
+        };
+        let check = |v: &Vec<_>| pow(pow(v[0], v[1], 10), v[2], v[3]) == target;
+        variables.iter()
+            .enumerate()
+            .filter_map(|(i, v)| check(v).then_some(i as i32))
+            .collect()
+    }
 }
